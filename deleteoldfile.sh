aa0ca45f5260fb89a24ff5981d972a9f082983eb
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 15:29:21 +0530

    calling script from antoher script

---
+++ deleteoldfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+filepath=/home/ec2-user/logs
+files=$(find filepath -name "*.log" -mtime +4)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ -d filepath]    # Checks that directory is exists or not.
+then
+    rm -rf files
+    echo -e "$G $files are deleted successfully $N"
+else
+    echo -e "$R $Files are not deleted $N"
+fi
