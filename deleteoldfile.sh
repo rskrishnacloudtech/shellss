2b4140315bc14df81bb5a73c6ae49a9b329169ce
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 16:04:13 +0530

    calling script from antoher script

--- deleteoldfile.sh
+++ deleteoldfile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 filepath="/home/ec2-user/logs"
-files=$(find $filepath -name "*.java" -mtime +4)
+files=($(find $filepath -name "*.java" -mtime +4))
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
 if [ -d $filepath ]    # Checks that directory is exists or not.
 then
-    if [ ${$files[@]} -gt 0 ]
+    if [ ${files[@]} -gt 0 ]
     then
         rm -rf $files
         echo -e "$G $files are deleted $N"
@@ -16,7 +16,7 @@ then
         echo -e "$R $files are NOT available $N"
     fi
 
-    if [ ${$files[@]} -eq 0 ]
+    if [ ${files[@]} -eq 0 ]
     then
         echo -e "$R $files are deleted successfully $N"
     fi
