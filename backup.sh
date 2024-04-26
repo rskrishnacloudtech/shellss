d8b4fc91dd1f8b3e0eeb175e75fdaecb8413e1db
Siva <rsk.sfdctraining1@gmail.com>
2024-04-27 00:31:01 +0530

    calling script from antoher script

--- backup.sh
+++ backup.sh
@@ -8,3 +8,4 @@ zipfile="backup-$timstamp.zip"
 
 #zip fromdir/backup-date.zip fromdir/zipfoder | mv fromdir/backup-date.zip todir/
 zip $zipfile ~/fromdir/zipfoder | mv $zipfile ~/todir/
+#zip zipfile.zip ~/fromdir/zipfoder | mv zipfile.zip ~/todir/
