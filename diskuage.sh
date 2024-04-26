a0bfa3b7cdd1b82387dbca5dde45d501feb7aa7b
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 18:38:43 +0530

    calling script from antoher script

--- diskuage.sh
+++ diskuage.sh
@@ -7,7 +7,7 @@ while IFS= read -r line
 do
     usage=$(echo $line | awk -d " " '{print $6F}' | cut -d "%" -f1)
     folder=$(echo $line | awk -d " " '{print $NF}')
-    if [ $usage -gt $dishthreshold ]
+    if [ $usage -ge $dishthreshold ]
     then
         echo "$folder is using more than $dishthreshold. Current memory is $usage"
     fi
