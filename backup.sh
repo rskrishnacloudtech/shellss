16be26ad405a631b80deb937c623e60eb816fbcf
Siva <rsk.sfdctraining1@gmail.com>
2024-04-27 00:18:14 +0530

    calling script from antoher script

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timstamp=$(date +%F-%H-%M-%S)
-zipfile=/home/ec2-user/backup-$timstamp.zip
+zipfile=fromdir/backup-$timstamp.zip
 #fromfolder="/home/ec2-user/logs/"
 #tofolder=/home/ec2-user/logs/
 #zip -r $zipfile $fromfolder | mv $zipfile $tofolder
