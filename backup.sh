cd710a08a31835b2ccbc2995562553e0e1a43615
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 23:40:41 +0530

    calling script from antoher script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo zip -r /home/ec2-user/logs.zip /home/ec2-user/logs/ | mv /home/ec2-user/logs.zip /home/ec2-user/logs/
+zipfile="/home/ec2-user/logs.zip"
+fromfolder="/home/ec2-user/logs/"
+tofolder=/home/ec2-user/logs/
+sudo zip -r $zipfile $fromfolder | mv $zipfile $tofolder
