cc4164a3f7d9b60df24d190c04b18f9aa2503a43
Siva <rsk.sfdctraining1@gmail.com>
2024-04-26 15:39:58 +0530

    calling script from antoher script

--- deleteoldfile.sh
+++ deleteoldfile.sh
@@ -6,13 +6,13 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-if [ -d $filepath ]    # Checks that directory is exists or not.
+if [ -d $filepath/$files ]    # Checks that directory is exists or not.
 then
     rm -rf $files
     echo -e "$G $files are deleted successfully $N"
     if [ ! -d $filepath/$files ]
     then
-        echo "Files are deleted successfully"
+        echo "$files are deleted successfully"
     fi
 else
     echo -e "$R $Files are not deleted $N"
