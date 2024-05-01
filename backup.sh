517b24382f93b4010a9e1119112c3f6d0af5272b
Siva <rsk.sfdctraining1@gmail.com>
2024-05-01 19:18:48 +0530

    dd

--- backup.sh
+++ backup.sh
@@ -2,10 +2,10 @@
 
 timstamp=$(date +%F-%H-%M-%S)
 zipfile="backup-$timstamp.zip"
-#fromfolder="/home/ec2-user/logs/"
-#tofolder=/home/ec2-user/logs/
+fromfolder="/home/ec2-user/logs/"
+tofolder=/home/ec2-user/logs/
 #zip -r $zipfile $fromfolder | mv $zipfile $tofolder
 
 #zip fromdir/backup-date.zip fromdir/zipfoder | mv fromdir/backup-date.zip todir/
-zip $zipfile ~/fromdir/zipfoder | mv $zipfile ~/todir/
+zip $tofolder/$zipfile $fromfolder/zipfoder
 #zip zipfile.zip ~/fromdir/zipfoder | mv zipfile.zip ~/todir/
